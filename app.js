26e12a90371d1a77b7c598a1dd8731d2eab679e6
yusuf <yusuferkoc99@gmail.com>
2020-12-22 02:45:23 +0300

    style fix

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 let textarea = document.getElementById('textarea')
+let lineNumbersBtn = document.getElementById('line-numbers-btn')
 let editor = CodeMirror.fromTextArea(textarea, {
-    lineNumbers: true
+    lineNumbers: true,
+    mode:'javascript'
   });
-editor.setOption('mode','javascript')
+
+let lineNumbers = true ;
+editor.setOption('mode','javascript');
 editor.setOption('theme','base16-dark');
+editor.setOption('lineNumbers',lineNumbers);
+
+lineNumbersBtn.addEventListener('click',(e) => {
+  lineNumbers =! lineNumbers;
+  editor.setOption('lineNumbers',lineNumbers);
+});
